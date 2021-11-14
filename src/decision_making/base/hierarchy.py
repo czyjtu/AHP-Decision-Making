a5fe38ee180eb12db97ad4c76d2244769b10360b
czyjtu <maciejczyjtu@gmail.com>
2021-11-14 17:41:07 +0100

    Added abstract method to the MCDA class

--- src/decision_making/base/hierarchy.py
+++ src/decision_making/base/hierarchy.py
@@ -1,9 +1,8 @@
 from src.decision_making.base import Criterium
 from typing import Sequence, TypeVar, Tuple
+from src.decision_making.base.mcda import MCDA, A
 
-from src.decision_making.base.mcda import MCDA
 
-A = TypeVar('A') # alternative type 
 
 class Hierarchy:
     def __init__(self, criteria:Sequence[Criterium]):
@@ -13,12 +12,13 @@ class Hierarchy:
 
 
     def rank_alternatives(self, alternatives:Sequence[A],  model:MCDA) -> Sequence[Tuple[A, float]]:
-        return list(map(lambda a: (a, self._rank(a, self.root_criteria, model)), alternatives))
+        ranked = list(map(lambda a: (a, self._rank(a, self.root_criteria, model)), alternatives))
+        return sorted(ranked, key=lambda item: -item[1])
 
 
     def rank_wrt_criterium(self, alternative:A, criterium:Criterium, model:MCDA) -> float:
         if criterium.is_leaf:
-            return alternative[Criterium.id] * model.weight_of(criterium)
+            return model.get_alternative_value(alternative, criterium) * model.weight_of(criterium)
         score = 0 
         for sub_id in alternative.sub_criteria:
             sub_score = self.rank_wrt_criterium(alternative, self.criteria[sub_id], model)
--- src/decision_making/base/mcda.py
+++ src/decision_making/base/mcda.py
@@ -1,9 +1,28 @@
 from src.decision_making.base import Criterium
-from typing import Sequence
+from typing import Sequence, TypeVar
 from abc import ABC, abstractmethod 
 
 
+A = TypeVar('A') # alternative type 
+
+
 class MCDA(ABC):
+    """
+    Abstract class defining interface for any Multiple-criteria decision analysis method
+    """
+    @abstractmethod
+    def get_alternative_value(self, alternative:A, criterium:Criterium) -> float:
+        """
+        Method calculate value of the given alternative with respect to the Criterium
+        """
+        pass
+
+
     @abstractmethod 
-    def rank_of(self, c:Criterium):
+    def priority_of(self, c:Criterium) -> float:
+        """
+        Method calculates priority of the given Criterium
+        :param c: Criterium 
+        :return priority: return float in range[0, 1] representing priority of the given criterium
+        """
         pass 
