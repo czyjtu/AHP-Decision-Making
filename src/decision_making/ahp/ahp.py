123058b0e6b39633b03de8dd6e8855df52e8dd00
kn2282 <57344058+kn2282@users.noreply.github.com>
2021-12-14 21:43:23 +0100

    GI complite

--- src/decision_making/ahp/ahp.py
+++ src/decision_making/ahp/ahp.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from ..base.mcda import MCDA, A
 from ..base.criterium import Criterium
@@ -10,7 +12,12 @@ class ComprehensionMatrix:
 
     def __init__(self, Matrix: np.matrix):
         self.matrix: np.matrix = Matrix
-        self.weights = np.array(0)
+        self.complete()
+        if self.__contain_zeros():
+            self.complete_comprehensions()
+        self.weights = self.calculate_weights()
+        self.cr = self.CR()
+        self.gi = self.GI()
 
     def complete(self) -> None:
         for i in range(len(self.matrix)):
@@ -42,7 +49,6 @@ class ComprehensionMatrix:
             return None
 
     def complete_comprehensions(self):
-        self.complete()
         t: bool = False
         to_complite = self.__zero_indexes()
         while self.__contain_zeros():
@@ -52,7 +58,7 @@ class ComprehensionMatrix:
                     "incomplete matrix!\n"
                     f"missing comprehensions: \n" +
                     repr({"one of: " + str(i) for i in missing})
-                                 )
+                )
             t = True
             i = 0
             while i < len(to_complite):
@@ -82,24 +88,33 @@ class ComprehensionMatrix:
         print(sub_graphs)
         missing = []
         for i in range(len(sub_graphs) - 1):
-            missing.append({(x, y) for x in sub_graphs[i] for y in sum(sub_graphs[i+1:], [])})
+            missing.append({(x, y) for x in sub_graphs[i] for y in sum(sub_graphs[i + 1:], [])})
         return missing
-
 
     def calculate_weights(self):
         matrix = np.array(self.matrix)
         suma = sum(matrix.reshape(-1))
-        self.weights = sum(matrix) / suma
+        return sum(matrix) / suma
 
     def CI(self) -> float:
-        self.complete()
-        # print(self.matrix)
-        self.calculate_weights()
         sum_vec = sum(np.array(self.matrix.transpose()))
         lambda_max = sum_vec @ self.weights
         n = len(self.matrix)
         ci = (lambda_max - n) / (n - 1)
         return ci
+
+    def GI(self):
+        """
+        Geometric Consistency Index
+        :return:
+        """
+        n = len(self.matrix)
+        total_log_error = 0
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                total_log_error += math.log10(self.matrix[i, j] * self.weights[j] / self.weights[i]) ** 2
+
+        return total_log_error * (2 / ((n - 1) * (n - 2)))
 
     def CR(self) -> float:
         # Consistency ratio, defined by Saaty
@@ -111,17 +126,18 @@ class ComprehensionMatrix:
 
 
 def compere(l: List[Dict], criteria_id) -> List[List]:
-    result=[]
+    result = []
     if type(l[0][criteria_id]) == bool:
         for i in range(len(l) - 1):
             for j in range(i + 1, len(l)):
                 result.append([l[i]['id'], l[j]['id'], comp_bool(l[j][criteria_id], l[i][criteria_id])])
     else:
         for i in range(len(l) - 1):
-            for j in range(i+1, len(l)):
+            for j in range(i + 1, len(l)):
                 result.append([l[i]['id'], l[j]['id'], l[j][criteria_id] / l[i][criteria_id]])
 
     return result
+
 
 def comp_bool(x, y):
     if (not x and not y) or (x and y):
@@ -129,7 +145,8 @@ def comp_bool(x, y):
     elif x:
         return 2
     else:
-        return 1/2
+        return 1 / 2
+
 
 class AHP(MCDA):
     def __init__(self, criteria: List[Criterium], comprehension_list: List[List], alternative_list: List[Dict]):
@@ -140,8 +157,6 @@ class AHP(MCDA):
             sorted_comprehensions[self.criteria_dict[com[0]].parent_criterium].append(com)
         self.matrices = {ids: ComprehensionMatrix(choice_list2matrix(x)) for ids, x in sorted_comprehensions.items()}
 
-        #TODO CRs to com_matrix attribute
-        self.CRs = {ids: matrix.CR() for ids, matrix in self.matrices.items()}
 
         self.weights = {}
         for ids in sorted_comprehensions.keys():
@@ -153,14 +168,14 @@ class AHP(MCDA):
         self.alternative_labels = sorted(list(map(lambda x: x['id'], alternative_list)))
         self.alternative_comprehension = dict()
         for cr_id in [cr.id for cr in criteria if cr.is_leaf]:
-            self.alternative_comprehension[cr_id] = ComprehensionMatrix(comp_list2matrix(compere(alternative_list, cr_id)))
+            self.alternative_comprehension[cr_id] = ComprehensionMatrix(
+                comp_list2matrix(compere(alternative_list, cr_id)))
             self.alternative_comprehension[cr_id].CR()
 
     def priority_of(self, c: Criterium) -> float:
         return self.weights[c.id] if not c.is_root else 1
 
-
-    def get_alternative_value(self, alternative:A, criterium:Criterium) -> float:
+    def get_alternative_value(self, alternative: A, criterium: Criterium) -> float:
         """
         Method getting value of single alternative in comprehension matrix of given criteria
         :param alternative:
@@ -187,6 +202,7 @@ def choice_list2matrix(choices: List[List]) -> np.matrix:
 
     return result_matrix
 
+
 def comp_list2matrix(choices: List[List]) -> np.matrix:
     n = num_of_points(len(choices))
     if n == 0: raise ValueError("Incorrect number of comprehensions!")
@@ -198,6 +214,7 @@ def comp_list2matrix(choices: List[List]) -> np.matrix:
         result_matrix[i, j] = comprehension[2]
 
     return result_matrix
+
 
 def num_of_points(N: int) -> int:
     n = 1
@@ -211,7 +228,8 @@ def num_of_points(N: int) -> int:
 def no_zero_index(l) -> set:
     return {i for i in range(len(l)) if l[i] != 0}
 
-def mix(s1, s2)-> Set[Set]:
+
+def mix(s1, s2) -> Set[Set]:
     return {{i, j} for i in s1 for j in s2}
 
 
@@ -220,14 +238,11 @@ def random_matrix(n, q):
     m = np.matrix(list(map(lambda x: list(map(lambda x1: float(int(x1)), x)), m)))
     return m
 
+
 def calc_RI(n, q, mt=25):
     CIs = [ComprehensionMatrix(random_matrix(n, q)).CI() for _ in range(mt)]
     return np.mean(CIs)
 
 
-
 if __name__ == '__main__':
     print(calc_RI(5, 9))
-
-
-
